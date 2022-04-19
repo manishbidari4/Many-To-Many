2480619d5995b578a7b20fe0a1a3b7b88eede924
manishbidari4 <101552530+manishbidari4@users.noreply.github.com>
2022-04-19 17:29:55 +0545

    Many To Many Mapping

---
+++ src/main/java/Springboot/ManyToMany/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package Springboot.ManyToMany.repository;
+
+import Springboot.ManyToMany.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long>{
+
+}
---
+++ src/main/java/Springboot/ManyToMany/repository/TagRepository.java
@@ -0,0 +1,10 @@
+package Springboot.ManyToMany.repository;
+import Springboot.ManyToMany.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long>{
+
+}
